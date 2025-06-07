1f1641a807044bf327f72cdbf7667ad13239010e
raju-kadachha <rajukadachha05@gmail.com>
2025-06-07 12:54:18 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,19 +1,15 @@
 export { cart, addToCart, removeFromCart }; //multiple variable,Functions
 //both are valid
 // export const cart = [];
-let cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    Quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    Quantity: 1
+let cart = JSON.parse(localStorage.getItem('cart'));
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart))
 }
-];
-
-
 //for adding items inside cart addToCart button
 // export function ...
 function addToCart(productId) {
+
     let matchingItem;
     // we looping through an array to check if matching item already exist or not
     cart.forEach((cartItem) => {
@@ -33,6 +29,7 @@ function addToCart(productId) {
             Quantity: 1
         });
     }
+    saveToStorage();
 }
 
 
@@ -44,5 +41,6 @@ function removeFromCart(productId) {
         }
     });
     cart = newCart;
+    saveToStorage();
 }
 console.log(cart);
